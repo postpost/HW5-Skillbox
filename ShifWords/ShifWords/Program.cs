8539969856b88df53a1f9a8ae4d7cbcc2c2500a4
polina_timofeeva <allein-p@yandex.ru>
2022-06-06 21:21:19 +0300

    Add project files.

---
+++ ShifWords/ShifWords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ShifWords
+{
+    internal class Program
+    {
+        static string ReverseWord(string inputPhrase)
+        {
+            string resultString = "";
+            string newString;
+            string [] reversedArr = SplitWords(inputPhrase);
+            for (int i = reversedArr.Length - 1; i >= 0; i--)
+            {
+                newString = reversedArr[i];
+                resultString += newString + " ";
+            }
+            Console.WriteLine(resultString);
+            return resultString;
+        }
+
+        static string[] SplitWords(string inputPhrase)
+        {
+            string[]input = inputPhrase.Split(' ');
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string inputPhrase = Console.ReadLine();
+            ReverseWord(inputPhrase);
+        }
+    }
+}
---
+++ SplitString/SplitString/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SplitString
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод, возвращающий массив из строковой переменной
+        /// </summary>
+        /// <param name="inputPhrase">Введенное предложение</param>
+        /// <returns>Возвращает массив слов</returns>
+        static string[] SplitString(string inputPhrase)
+        {
+            
+            string[] arr = inputPhrase.Split(' ');
+            PrintArray(arr);
+            return arr;
+
+        }
+
+        /// <summary>
+        /// Метод, выводящий массив в консоль
+        /// </summary>
+        /// <param name="inputString">Массив, который необходимо распечатать</param>
+        static void PrintArray(string[] inputString)
+        {
+            foreach (var e in inputString) Console.WriteLine(e);
+        }
+
+        ////static void Split01(params string[] arr)
+        ////{
+        ////    Console.WriteLine("Type a sentence");
+        ////    string input = Console.ReadLine();
+        ////    var word = string.Empty;
+        ////    for (int j = 0; j < input.Length; j++)
+        ////    {
+        ////        if (input[j] != ' ')
+        ////        {
+        ////            word += input[j];
+        ////        }
+        ////        else 
+        ////        {
+        ////            Console.WriteLine();
+        ////            arr[j] = word;
+        ////        }
+        ////    PrintArray(arr);
+        ////}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string inputPhrase = Console.ReadLine();
+            SplitString(inputPhrase);
+            ////Split01();
+        }
+    }
+}
+
